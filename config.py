68d60e2765a3a9ea0bf78b30b9dcbf73080dd09b
ensured <74529637+ensured@users.noreply.github.com>
2021-06-15 17:44:33 -0700

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,17 @@ api_secret = ""
 access_token = ""
 access_token_secret = ""
 
+# amount of posts to get before rerunning the function 'fav_and_retweet()'
+# the larger the number the farther back the tweets you get will be (time wise)
+amount_of_posts_to_get = 10
+
+# enter as many hashtags here as you want
+search_a = "#CardanoNFT"
+search_b = "#ADA"
+search_c = "#Cardano"
+search_d = "#CardanoCommunity"
+search_e = "#CardanoFamily"
+search_f = "#CNTF"
 
 # blacklisted people here
 blacklisted_usernames = (
