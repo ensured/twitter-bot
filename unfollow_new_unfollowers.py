b50ce507a1ff114fa8d17454d5770744b72ac45b
ensured <74529637+ensured@users.noreply.github.com>
2021-06-20 16:50:34 -0700

    Update unfollow_new_unfollowers.py

--- unfollow_new_unfollowers.py
+++ unfollow_new_unfollowers.py
@@ -16,7 +16,6 @@ auth = tweepy.OAuthHandler(api_key, api_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-# ------> change this to your twitter username <-------------
 followers = api.followers_ids(twitter_username)
 friends = api.friends_ids(twitter_username)
 
