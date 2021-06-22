a2cad4f8cdb5cc83894adec79fbe6386d9971e59
ensured <74529637+ensured@users.noreply.github.com>
2021-06-21 17:21:19 -0700

    convert tuple to set for faster membership testing

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-# your twitter username goes here
+# your twitter username
 twitter_username = ""
 
-# your api details go here ↓↓↓
+# your api details ↓↓↓
 api_key = ""
 api_secret = ""
 access_token = ""
@@ -11,7 +11,7 @@ access_token_secret = ""
 # the larger the number the farther back the tweets you get will be (time wise)
 amount_of_posts_to_get = 15
 
-# enter as many hashtags here as you want (may need to update twitter_bot.py with the added or removed values)
+# enter as many hashtags as you want (may need to update twitter_bot.py with the added or removed values)
 search_a = "#CardanoNFT"
 search_b = "#ADA"
 search_c = "#Cardano"
@@ -75,7 +75,7 @@ blacklisted_hashtags_for_twitter_bot = {
     "#DOGE",
 }
 
-# blacklisted people here
+# blacklisted people
 blacklisted_usernames_for_twitter_bot = {
     "topstonks",
     "chaintoday1",
